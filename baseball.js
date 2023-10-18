2ec8a01cf18d94db0e5aef9930dbc33c611a3bb8
heyjk2212 <jinakim2212@gmail.com>
2023-10-18 13:56:14 +0900

    add validation

--- baseball.js
+++ baseball.js
@@ -19,37 +19,48 @@ uniqueNums = uniqueNums.join('')
 
 const prompt = require("prompt-sync")();
 
-
 let strike = false;
 let count = 1
 
 console.log('[test] random number: ', uniqueNums)
 
 while(strike !== true){
-
+    
     let guess = prompt(`${count}번째 시도 :`)
 
-    let countS = 0;
-    let countB = 0;
+    // 유저가 입력한 값이 숫자인 문자열인지 확인
+    if (/^\d+$/.test(guess)) {
 
-    temp = []
+        // 유저가 입력한 값이 3개의 숫자가 맞는지 확인
+        if(guess.length === 3){
+            
+            let countS = 0;
+            let countB = 0;
 
-    for(let i = 0; i < uniqueNums.length; i++) {
-        if(guess === uniqueNums){
-            strike = true
-        }else if(guess[i] === uniqueNums[i]) {
-            countS += 1
-        }else if(uniqueNums.includes(guess[i])){ 
-            countB += 1
+            temp = []
+
+            for(let i = 0; i < uniqueNums.length; i++) {
+                if(guess === uniqueNums){
+                    strike = true
+                }else if(guess[i] === uniqueNums[i]) {
+                    countS += 1
+                }else if(uniqueNums.includes(guess[i])){ 
+                    countB += 1
+                }
+            }
+
+            if(strike === true){
+                console.log(`3S\n${count}번만에 맞히셨습니다.\n게임을 종료합니다.`)
+            }else if(countB === 3) {
+                console.log(`${countB}B`)
+            }else{
+                console.log(`${countB}B${countS}S`)
+            }
+        }else{
+            console.log('정확히 숫자 3개를 입력해주세요.')
         }
-    }
-
-    if(strike === true){
-        console.log(`3S\n${count}번만에 맞히셨습니다.\n게임을 종료합니다.`)
-    }else if(countB === 3) {
-        console.log(`${countB}B`)
     }else{
-        console.log(`${countB}B${countS}S`)
+        console.log('틀렸습니다. 0에서 9까지인 숫자만 입력하세요.')
     }
 
     count += 1
